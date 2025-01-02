d3e27614a3a6f95a3aedac40467d6643f2ba44d3
tmittal24 <67136594+tmittal24@users.noreply.github.com>
2025-01-02 19:11:36 +0530

    Add files via upload

---
+++ classes/ex4/MyClass.cpp
@@ -0,0 +1,54 @@
+#include "MyClassTemplate.h"
+#include <iostream>
+using namespace std;
+
+// class Student;
+
+void printDetails(Student myObj);
+int main()
+{
+  {
+    Student myObj1; // Create an object of MyClass
+
+    // Access attributes and set values
+    myObj1.age = 18;
+    myObj1.name = "Trijal";
+    myObj1.myMethod();
+    printDetails(myObj1);
+
+    // Access static variable
+    Student::group = "A";
+  }
+
+  {
+    Student myObj2("Ritu", 4); // Create an object of MyClass
+    // Access static variable
+    Student::group = "B";
+
+    // Print attribute values
+    printDetails(myObj2);
+  }
+
+  {
+    // Create an object of MyClass using new keyword
+    Student *myObj3 = new Student("shammi", 5);
+
+    // Access static variable
+    Student::group = "C";
+
+    // Print attribute values
+    printDetails(*myObj3);
+
+    cout << "--------------------------------" << "\n";
+  }
+
+  return 0;
+}
+
+void printDetails(Student myObj)
+{
+  // Print attribute values
+  cout << "--------------------------------" << "\n";
+  cout << myObj.age << "\n";
+  cout << myObj.name << "\n";
+}
