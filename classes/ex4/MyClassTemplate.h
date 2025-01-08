5f67b7f0c979d78c67e4897e244b15402e7475ac
tmittal24 <67136594+tmittal24@users.noreply.github.com>
2025-01-08 12:23:37 +0530

    Update MyClassTemplate.h

--- classes/ex4/MyClassTemplate.h
+++ classes/ex4/MyClassTemplate.h
@@ -3,10 +3,14 @@ using namespace std;
 
 class Student // The class
 {
-public:        // Access specifier
-  int age;     // Attribute (int variable)
-  string name; // Attribute (string variable)
+public:           // Access specifier
+  int age;        // Attribute (int variable)
+  string name;    // Attribute (string variable)
+  string message; // Attribute (string variable)
+
+  // static variable - public access
   static string group;
+
   // inline constructor declaration
   Student()
   {
@@ -14,12 +18,25 @@ public:        // Access specifier
   }
 
   // external constructor declaration (with arguments)
-  Student(string a_name, int a_age);
+  Student(string name, int age);
+
+  // Constructor definition outside the class
+  Student(string name, int age, string message)
+  {
+    this->name = name;
+    this->age = age;
+    this->message = message;
+    cout << "Hello World! from external constructor.... \n";
+  }
 
   // external method
   void myMethod();
 };
 
+// static variable - definition
+string Student::group = "";
+
+// Constructor definition outside the class
 Student::Student(string a_name, int a_age)
 {
   name = a_name;
@@ -32,5 +49,3 @@ void Student::myMethod()
 {
   cout << "Hello World! from external myMethod.... \n";
 }
-
-string Student::group;
